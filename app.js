672c7d1e49965a2930c1cb6b05cf5e86f26bf480
loryuen <56411330+loryuen@users.noreply.github.com>
2020-03-09 17:16:31 -0400

    connected with dropdown

--- app.js
+++ app.js
@@ -26,24 +26,39 @@ function handleSelect() {
 
     // build plot based on selected stock
 
-    buildPlot(id);
+    optionChanged(id);
 }
 
 // function to render plots
-function buildPlot(id) {
+function optionChanged(id) {
     d3.json("data/samples.json").then((data) => {
     console.log(data);
+    //console.log(id);
     
-    var id = data.samples.map( x => x.id);
+    for (var i = 0; i<data.samples.length; i++) {
+        //console.log('testtest')
+        //console.log(data.samples[i]['id'])
+        if (data.samples[i]['id'] === id) {
+            console.log(data.samples[i])
+            var otu_ids = data.samples[i]['otu_ids']
+            console.log(otu_ids)
+            var sample_values = data.samples[i]['sample_values']
+            console.log(sample_values)
+        }
+    }
+    
+
     // define based on id
-    var sample_values = data.samples.map(x => x.sample_values);
-    var otu_ids = data.samples.map( x => x.otu_ids);
-    console.log(sample_values);
-    console.log(otu_ids);
+    //var sample_values = data.samples.map(x => x.sample_values);
+    //var otu_ids = data.samples.map( x => x.otu_ids);
+    //console.log(sample_values);
+    //console.log(otu_ids);
+
+    //console.log(data.samples[0]);
 
     var trace1 = {
         x: sample_values,
-        y: otu_ids,
+        //y: otu_ids,
         type: "bar",
         name: data.samples.otu_labels
     };
