b67fe4aa682fa69dc46a6eb807ed98ebf008d6df
loryuen <56411330+loryuen@users.noreply.github.com>
2020-03-10 20:44:41 -0400

    bubble

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-console.log('hi1')
 
 // append option to #selDataset and loop through patient ID numbers to assign as values and text labels to options for drop down menu
 function buildDropdown() {
     d3.json("data/samples.json").then((data) => {
         var id = data.samples.map( x => x.id);
+        //var id = data.names.map(x=>x)
         console.log(id);
-        console.log('hello');
 
         var section = d3.select('#selDataset');
         for (var i=0; i<id.length; i++) {
@@ -35,7 +34,8 @@ function optionChanged(id) {
     console.log(data);
     //console.log(id);
 
-    
+    //demographicInfo(id);
+
     for (var i = 0; i<data.samples.length; i++) {
         // loop through to get otu and sample values for each subject selected
         if (data.samples[i]['id'] === id) {
@@ -84,36 +84,107 @@ function optionChanged(id) {
 
 }
 
-// metadata demographic info
+// function for metadata demographic info
+//function demographicInfo() {
+    d3.json("data/samples.json").then((data) => {
+        console.log(data);
+        
+            for (var i = 0; i<data.metadata.length; i++) {
+                // loop through to get metadata to get the one for subject selected
+                if (data.metadata[i]['id'] === 940) {
+                    console.log(data.metadata[i])
+                    var idNo = data.metadata[i]['id']
+                    var ethnicity = data.metadata[i]['ethnicity']
+                    var gender = data.metadata[i]['gender']
+                    var age = data.metadata[i]['age']
+                    var location = data.metadata[i]['location']
+                    var bbtype = data.metadata[i]['bbtype']
+                    var wfreq = data.metadata[i]['wfreq']
 
+                    metadataList = [`id: ${idNo}`, `ethnicity: ${ethnicity}`, `gender: ${gender}`, `age: ${age}`, `location: ${location}`, `bbtype: ${bbtype}`, `wfreq: ${wfreq}`]
+
+                    var demoBox = d3.select('#sample-metadata')
+                    //var demoList = demoBox.append('')
+
+                    // var demoList = demoBox.append('par')
+                    // var hi = Object.entries(data.metadata[i]).forEach( ([key,value]) => {
+                    //     console.log(key,value)})
+                    // demoList.append(hi)
+
+                    metadataList.forEach( (x) => {
+                        var demoList = demoBox.append('li')
+                        demoDisp = demoList.text(x)
+                        Object.entries(data.metadata[i]).forEach( ([key,value]) => {
+                            console.log(key,value)
+                        })
+                    });
+                    
+
+                    }
+
+                    
+                    
+                }
+        }
+        
+    )
+//};
+//demographicInfo();
+
+
+// bubble graph
 d3.json("data/samples.json").then((data) => {
     console.log(data);
-    
-        for (var i = 0; i<153; i++) {
-            // loop through to get metadata to get the one for subject selected
-            if (data.metadata[i]['id'] === 940) {
-                console.log(data.metadata[i])
-                var id = data.metadata[i]['id']
-                var ethnicity = data.metadata[i]['ethnicity']
-                var gender = data.metadata[i]['gender']
-                var age = data.metadata[i]['age']
-                var location = data.metadata[i]['location']
-                var bbtype = data.metadata[i]['bbtype']
-                var wfreq = data.metadata[i]['wfreq']
+    //console.log(id);
 
-                metadataList = [`id: ${id}`, `ethnicity: ${ethnicity}`, `gender: ${gender}`, `age: ${age}`, `location: ${location}`, `bbtype: ${bbtype}`, `wfreq: ${wfreq}`]
+    for (var i = 0; i<data.samples.length; i++) {
+        // loop through to get otu and sample values for each subject selected
+        if (data.samples[i]['id'] === 940) {
+            console.log(data.samples[i])
 
-                var demoBox = d3.select('#sample-metadata')
-                //var demoList = demoBox.append('')
-                metadataList.forEach( (x) => {
-                    var demoList = demoBox.append('li')
-                    demoDisp = demoList.text(x)
-                }
-                    )
-                
-            }
+            //get otu IDs
+            var otu_ids = data.samples[i]['otu_ids']
+            console.log(otu_ids)
+            //var otu_idsTop = data.samples[i]['otu_ids'].sort( (a,b) => b.otu_idsTop - a.otu_idsTop)
+            //var otu_ids10 = otu_idsTop.slice(0,10)
+
+            //labels list for OTUs
+            // var labels =[]
+            // otu_ids10.forEach(x=> labels.push(`OTU ${x}`.toString()))
+            // console.log(labels)
+
+            // get sample values
+            var sample_valuesList = data.samples[i]['sample_values']
+            console.log(sample_valuesList)
+            //var sample_values = sample_valuesList.slice(0,10)
+            //console.log(sample_values)
         }
     }
-)
+
+    // make bar chart
+    var trace2 = {
+        x: otu_ids,
+        y: sample_valuesList,
+        //text: labels,
+        //hovertext?
+       // mode: 'markers',
+        type: "bubble",
+        //marker: sample_valuesList
+        //marker: sample_valuesList
+        //name: data.samples.otu_labels
+    };
+    
+    var data = [trace2];
+
+    var layout = {
+        title: "Number of Samples by OTU",
+        xaxis: { title: "OTU ID" },
+        yaxis: { title: "Number of samples" }
+    };
+
+    Plotly.newPlot("bubble", data, layout);
+
+    });
+
 
 d3.select("#selDataset").on("click", handleSelect);
